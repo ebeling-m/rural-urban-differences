f25f88b36f3864dbf1504abf69c0d5345f19cf76
Marcus Ebeling <marcus.ebeling@ki.se>
2021-05-10 14:58:50 +0200

    Mapvalues function added

--- packages_functions.r
+++ packages_functions.r
@@ -2,3 +2,28 @@
 library(xlsx)
 library(tidyverse)
 
+
+# Additional Function borrowed from plyr package
+
+mapvalues <- function (x, from, to, warn_missing = TRUE) 
+{
+  if (length(from) != length(to)) {
+    stop("`from` and `to` vectors are not the same length.")
+  }
+  if (!is.atomic(x)) {
+    stop("`x` must be an atomic vector.")
+  }
+  if (is.factor(x)) {
+    levels(x) <- mapvalues(levels(x), from, to, warn_missing)
+    return(x)
+  }
+  mapidx <- match(x, from)
+  mapidxNA <- is.na(mapidx)
+  from_found <- sort(unique(mapidx))
+  if (warn_missing && length(from_found) != length(from)) {
+    message("The following `from` values were not present in `x`: ", 
+            paste(from[!(1:length(from) %in% from_found)], collapse = ", "))
+  }
+  x[!mapidxNA] <- to[mapidx[!mapidxNA]]
+  x
+}
