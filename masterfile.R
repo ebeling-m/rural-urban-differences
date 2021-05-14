8cfc6241705490eb96c1d7b7e659ca2b452c94d9
Marcus Ebeling <marcus.ebeling@ki.se>
2021-05-15 00:54:02 +0200

    "source-cing" libraries and Fct, dat prep Germany and dat prep ENW

---
+++ masterfile.R
@@ -0,0 +1,18 @@
+# This is the masterfile for the analysis of 
+# rural-urban mortality differences in Germany and England & Wales
+
+# Packages and Functions: 
+# This code contains all packages and functions for the analysis 
+source("packages_functions.r")
+
+# Step 1: Prepare data for Germany for NUTS-3 regions
+# Data has been obtained from Regionalstatistik.de, Eurostat, and 
+# Individual Cause of death data (restricted access) prepared by 
+# M. Mühlichen
+source("data_preparation_Germany.R")
+
+# Step 2: Prepare data for England & Wales for LAU-1 regions
+# Data has been obtained from https://www.nomisweb.co.uk/sources
+# and the census 2011
+source("data_preparation_EnglandWales.R")
+
