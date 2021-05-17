241fd4fcb8ffcfc5a33ad6c20824acbc859b39d7
Marcus Ebeling <marcus.ebeling@ki.se>
2021-05-18 01:02:32 +0200

    Extended towards unadjusted analysis

--- masterfile.R
+++ masterfile.R
@@ -9,10 +9,18 @@ source("packages_functions.r")
 # Data has been obtained from Regionalstatistik.de, Eurostat, and 
 # Individual Cause of death data (restricted access) prepared by 
 # M. Mühlichen
-source("data_preparation_Germany.R")
+source("data_preparation_Germany.R", encoding = "UTF-8")
+# Central object: datGER
 
 # Step 2: Prepare data for England & Wales for LAU-1 regions
 # Data has been obtained from https://www.nomisweb.co.uk/sources
 # and the census 2011
 source("data_preparation_EnglandWales.R")
+# Central object: datENW
+
+# Step 3: Correlation LE and pop density Figure
+source("scatter_LE_PD.R")
+
+# Step 4: Unadjusted rural-urban differences
+
 
